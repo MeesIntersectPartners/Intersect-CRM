c0e0c3816f944c8c654df288ae8f9d9d99b5c9f8
MeesIntersectPartners <mees@intersect-partners.com>
2026-04-08 12:12:13 +0200

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,10 +1,7 @@
-// /api/auth/login
-// Stuurt gebruiker door naar Microsoft login pagina
-
 const { getAuthUrl } = require('../../lib/microsoft');
 
-export default function handler(req, res) {
-  const { user } = req.query; // bijv. MA of JK
+module.exports = function handler(req, res) {
+  const { user } = req.query;
   const authUrl = getAuthUrl(user || 'unknown');
   res.redirect(authUrl);
-}
+};
